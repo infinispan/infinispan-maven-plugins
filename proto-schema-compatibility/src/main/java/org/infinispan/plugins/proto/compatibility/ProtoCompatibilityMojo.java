d8dd8a5f72c90302098ac99f917e470897e7666b
Tristan Tarrant <ttarrant@redhat.com>
2024-05-15 16:10:31 +0200

    ISPN-16080 Use ProtoStream for ProtoLock

--- proto-schema-compatibility/src/main/java/org/infinispan/plugins/proto/compatibility/ProtoCompatibilityMojo.java
+++ proto-schema-compatibility/src/main/java/org/infinispan/plugins/proto/compatibility/ProtoCompatibilityMojo.java
@@ -1,33 +1,29 @@
-/*
- *  Copyright (c) 2018, salesforce.com, inc.
- *  All rights reserved.
- *  Licensed under the BSD 3-Clause license.
- *  For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
- */
 package org.infinispan.plugins.proto.compatibility;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFileAttributeView;
-import java.nio.file.attribute.PosixFilePermissions;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.project.MavenProject;
+import org.infinispan.protostream.FileDescriptorSource;
+import org.infinispan.protostream.config.Configuration;
+import org.infinispan.protostream.descriptors.FileDescriptor;
+import org.infinispan.protostream.descriptors.ProtoLock;
+import org.infinispan.protostream.impl.parser.ProtostreamProtoParser;
 
 /**
- * Modified version of the https://github.com/salesforce/proto-backwards-compat-maven-plugin, that does not require
- * proto.lock files to be created inline with .proto files and allows execution to be skipped.
+ * Protocol Buffers compatibility checks using ProtoStream
  *
  * @author Ryan Emerson
  * @since 10.0
@@ -38,10 +34,6 @@ import org.apache.maven.project.MavenProject;
       requiresDependencyResolution = ResolutionScope.COMPILE,
       threadSafe = true)
 public class ProtoCompatibilityMojo extends AbstractMojo {
-
-   @Parameter(defaultValue = "${project}", readonly = true, required = true)
-   private MavenProject mavenProject;
-
    @Parameter(defaultValue = "${basedir}")
    private String protoLockRoot;
 
@@ -53,42 +45,10 @@ public class ProtoCompatibilityMojo extends AbstractMojo {
 
    /**
     * Execute the plugin.
+    *
     * @throws MojoExecutionException thrown when execution of protolock fails.
-    * @throws MojoFailureException thrown when compatibility check fails.
     */
-   public void execute() throws MojoExecutionException, MojoFailureException {
-      Path exeDirPath = Paths.get(mavenProject.getBuild().getDirectory(), "protolock-bin");
-      if (!Files.isDirectory(exeDirPath)) {
-         try {
-            Files.createDirectory(exeDirPath);
-         } catch (IOException e) {
-            throw new MojoExecutionException("Unable to create the protolock binary directory", e);
-         }
-      }
-
-      String classifier = mavenProject.getProperties().getProperty("os.detected.classifier");
-      String exeExtension = classifier.startsWith("windows") ? ".exe" : "";
-      Path exePath = exeDirPath.resolve("protolock" + exeExtension);
-      if (!Files.exists(exePath)) {
-         String protolockResourcePath = classifier + "/protolock" + exeExtension;
-
-         try (InputStream in = this.getClass().getClassLoader().getResourceAsStream(protolockResourcePath)) {
-            if (in == null) {
-               getLog().warn(String.format("OS not supported. Unable to find a protolock binary for the classifier '%s'. Skipping compatibility check.", classifier));
-               return;
-            }
-
-            Files.copy(in, exePath);
-
-            PosixFileAttributeView attributes = Files.getFileAttributeView(exePath, PosixFileAttributeView.class);
-            if (attributes != null) {
-               attributes.setPermissions(PosixFilePermissions.fromString("rwxrwxr-x"));
-            }
-         } catch (IOException e) {
-            throw new MojoExecutionException("Unable to write the protolock binary", e);
-         }
-      }
-
+   public void execute() throws MojoExecutionException {
       try {
          Path lockFile = Paths.get(protoLockRoot, "proto.lock");
          boolean lockFileExists = Files.exists(lockFile);
@@ -96,38 +56,40 @@ public class ProtoCompatibilityMojo extends AbstractMojo {
             getLog().info("Ignoring protolock check as there isn't an existing proto.lock file and commitProtoLock=false.");
             return;
          }
-
+         Path protoRoot = Paths.get(protoSourceRoot);
+         List<Path> protoFiles;
+         try (Stream<Path> stream = Files.walk(protoRoot)) {
+            protoFiles = stream.filter(p -> p.getFileName().toString().endsWith(".proto")).toList();
+         }
+         FileDescriptorSource fds = new FileDescriptorSource();
+         for (Path p : protoFiles) {
+            fds.addProtoFile(protoRoot.relativize(p).toString(), p.toFile());
+         }
+         ProtostreamProtoParser parser = new ProtostreamProtoParser(Configuration.builder().build());
+         Map<String, FileDescriptor> descriptors = parser.parse(fds);
+         ProtoLock protoNew = new ProtoLock(descriptors.values());
          if (!lockFileExists) {
-            runCmd(exePath, "init");
+            try (OutputStream os = Files.newOutputStream(lockFile)) {
+               protoNew.writeLockFile(os);
+            }
             getLog().info("Initialized protolock.");
          } else {
-            runCmd(exePath, "status");
+            ProtoLock protoOld;
+            try (InputStream is = Files.newInputStream(lockFile)) {
+               protoOld = ProtoLock.readLockFile(is);
+            }
+            protoOld.checkCompatibility(protoNew, true);
             getLog().info("Backwards compatibility check passed.");
 
             if (commitProtoLock) {
-               runCmd(exePath, "commit");
+               try (OutputStream os = Files.newOutputStream(lockFile)) {
+                  protoNew.writeLockFile(os);
+               }
                getLog().info("Schema changes committed to proto.lock.");
             }
          }
-      } catch (IOException | InterruptedException e) {
+      } catch (IOException e) {
          throw new MojoExecutionException("An error occurred while running protolock", e);
       }
-   }
-
-   private void runCmd(Path exePath, String action) throws InterruptedException, IOException, MojoFailureException {
-      String cmd = String.format("%s %s --lockdir %s --protoroot %s", exePath, action, protoLockRoot, protoSourceRoot);
-      Process protolock = Runtime.getRuntime().exec(cmd, null, mavenProject.getBasedir());
-      BufferedReader stdInput = new BufferedReader(new InputStreamReader(protolock.getInputStream()));
-
-      String errorMsg;
-      StringBuilder sb = new StringBuilder();
-      while ((errorMsg = stdInput.readLine()) != null) {
-         getLog().error(errorMsg);
-         sb.append(errorMsg).append("\n");
-      }
-
-      int exitVal = protolock.waitFor();
-      if (exitVal != 0)
-         throw new MojoFailureException(String.format("Backwards compatibility check failed with: %s", sb.toString()));
    }
 }
